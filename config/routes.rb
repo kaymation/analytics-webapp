ab97fd5579003f0e6e34a7dd3391d8cdba6f6da1
Kevin Quigley <kevinmquigley.41@gmail.com>
2014-12-31 21:22:56 +0000

    apitest

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,14 @@ Cheftab::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'restaurants#index'
 
-  get 'apitest' => 'reports#new'
+  get '/apitest' => 'reports#new'
 
   post '/reports/:user_id/:restaurant_id', to: 'reports#create'
 
   get '/contact', to: 'inquiry#index'
 
   get '/inquiry/send_mail', to: 'inquiry#send_mail'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
