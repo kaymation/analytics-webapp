315956db66519e2f9cb0a70dcd64a0bd845864d4
Kevin Quigley <kevinmquigley.41@gmail.com>
2015-03-15 02:17:31 +0000

    got rid of wonky setup

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-:address => "smtp.mandrillapp.com",
-:port => 587,
-:domain => "mandrillapp.com",
-:user_name => "app31777222@heroku.com",
-:password => "5OunalrBFKaYWKZG1ZFAwg",
-:authentication => "login",
-:enable_starttls_auto => true
-}
