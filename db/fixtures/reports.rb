6632f023e60909861e2105d407f801600536baaa
Kevin Quigley <kevinmquigley.41@gmail.com>
2015-03-15 15:59:12 +0000

    seed for test user on prod build

--- db/fixtures/reports.rb
+++ db/fixtures/reports.rb
@@ -1,13 +1,15 @@
 Report.destroy_all
-
+if User.where(email: 'test@example.com').nil?
+	User.create(email: 'test@example.com', password: 'password' )
+end
 100.times do
 	Report.seed do |r|
 		r.item = ["Small Burger", "Medium Burger", "Large Burger"].sample
 		r.when = rand(2.years).seconds.ago
 		r.preptime = rand(1000)
 		r.device_id = 1
-		r.restaurant_id = Restaurant.maximum(:created_at)
-		r.user_id = 2
+		r.restaurant_id = Restaurant.all.sort_by(:created_at).first
+		r.user_id = User.where(email: 'test@example.com').first.id
 		r.order_number = rand(100)
 	end
 end
