5b09221c17c6f4b420c592e4a77c509db4275515
Kevin Quigley <kevinmquigley.41@gmail.com>
2015-03-15 16:44:07 +0000

    reset settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Cheftab::Application.configure do
 }
 
 # ActionMailer Config
-config.action_mailer.default_url_options = { :host => 'cheftabcloud.heroku.com' }
+config.action_mailer.default_url_options = { :host => 'cheftabcloud.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
