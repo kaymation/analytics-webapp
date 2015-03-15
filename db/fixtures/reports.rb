9e11a762e326589f9128a77c36b368d71df904b5
Kevin Quigley <kevinmquigley.41@gmail.com>
2015-03-15 16:08:39 +0000

    new test user

--- db/fixtures/reports.rb
+++ db/fixtures/reports.rb
@@ -1,5 +1,5 @@
 Report.destroy_all
-if User.where(email: 'test@example.com').nil?
+if User.where(email: 'test_user@example.com').nil?
 	 u = User.create(email: 'test@example.com', password: 'password' )
 	 u.save!
 end
@@ -10,7 +10,7 @@ end
 		r.preptime = rand(1000)
 		r.device_id = 1
 		r.restaurant_id = Restaurant.all.sort_by(:created_at).first
-		r.user_id = User.where(email: 'test@example.com').first.id
+		r.user_id = User.where(email: 'test_user@example.com').first.id
 		r.order_number = rand(100)
 	end
 end
