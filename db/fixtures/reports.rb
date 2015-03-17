0d694eca70301fad11ded9093e623ba92c795dfe
Kevin Quigley <kevinmquigley.41@gmail.com>
2015-03-17 19:09:59 +0000

    seeds for me

--- db/fixtures/reports.rb
+++ db/fixtures/reports.rb
@@ -6,7 +6,7 @@ Report.destroy_all
 		r.preptime = rand(1000)
 		r.device_id = 1
 		r.restaurant_id = Restaurant.all.order(:created_at).first.id
-		r.user_id = User.where(email: 'mvpetronaci@gmail.com').first.id
+		r.user_id = User.where(email: 'kevinmquigley.41@gmail.com').first.id
 		r.order_number = rand(100)
 	end
 end
