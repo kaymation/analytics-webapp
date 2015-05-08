4f189c7e781333b467120b6b3239290a20b7fd58
Kevin Quigley <kevinmquigley.41@gmail.com>
2015-05-08 18:16:27 +0000

    to seed demo user

--- db/fixtures/reports.rb
+++ db/fixtures/reports.rb
@@ -3,8 +3,8 @@
 		r.item = ["Small Burger", "Medium Burger", "Large Burger"].sample
 		r.when = rand(2.years).seconds.ago
 		r.preptime = rand(1000)
-		r.device_id = 1
-		r.restaurant_id = 1
+		r.device_id = 2
+		r.restaurant_id = 6
 		r.user_id = User.where(email: 'demo@example.com').first.id
 		r.order_number = rand(100)
 	end
